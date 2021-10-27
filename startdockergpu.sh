90ba67689c5df7583d6afe7011eb098a09c9ea26
ammaryasirnaich <ammar.naich@gmail.com>
2021-10-27 11:59:42 +0100

    Update startdockergpu.sh
    different unsuccessful attempts for the configuration

--- startdockergpu.sh
+++ startdockergpu.sh
@@ -5,3 +5,11 @@ xhost local:root
 # xhost +SI:localuser:root
 # xhost +SI:localuser:<username>
 docker run -it --rm -e DISPLAY=${DISPLAY} --gpus all --net=host --ipc=host --privileged -env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix --name gpu_machine dl_playland:gpu
+
+
+# docker run -it --rm -e DISPLAY=${DISPLAY} --runtime=nvidia --net=host --ipc=host --privileged -env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix --name gpu_machine dl_playland:gpu
+
+#docker run --runtime=nvidia --net=host --ipc=host --privileged --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --name gpu_machine dl_playland:gpu
+#docker run -it --name gpu_machine -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix dl_playland:gpu
+
+#docker run --runtime=nvidia --net=host --ipc=host --privileged --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --name gpu_machine dl_playland:gpu
